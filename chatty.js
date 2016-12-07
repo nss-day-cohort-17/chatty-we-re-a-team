2f50ecab3272eedf3da1cf941300b8892bd96bfd
j-west <westja89@gmail.com>
2016-12-07 13:30:52 -0600

    Uncomment three lines and add correct element ID's to them to add message from input field to main-body div

--- chatty.js
+++ chatty.js
@@ -3,9 +3,9 @@ document.querySelector("#message_id").addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     var userMessage = document.querySelector("input").value;
     console.log(userMessage);
-    // var newMessage = document.createElement("Element to hold message content")
-    // newMessage.innerHTML = userMessage;
-    //document.querySelector(" main body element that holds all messages").appendChild(newMessage);
+    var newMessage = document.createElement("p")
+    newMessage.innerHTML = userMessage;
+    document.querySelector("#main-body").appendChild(newMessage);
   }
 
 })
