7289295af115b8cd8c1b6a79f38b579b954c2bb4
j-west <westja89@gmail.com>
2016-12-07 13:37:38 -0600

    Add line to clear input field value on enter keypress

--- chatty.js
+++ chatty.js
@@ -2,9 +2,11 @@ document.querySelector("#message_id").addEventListener("keypress", (e) => {
   // console.log(e);
   if (e.key === "Enter") {
     var userMessage = document.querySelector("input").value;
+    document.querySelector("input").value = "";
     console.log(userMessage);
     var newMessage = document.createElement("p")
     newMessage.innerHTML = userMessage;
+
     document.querySelector("#main-body").appendChild(newMessage);
   }
 
