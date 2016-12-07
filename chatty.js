1c8ed7f3c4c3fa8633f226d3da7ba615dd698d1b
j-west <westja89@gmail.com>
2016-12-07 16:56:46 -0600

    Add addAttribute and toggleClass functions that are called when a checkbox is clicked

--- chatty.js
+++ chatty.js
@@ -1,3 +1,28 @@
+let bodyVar = document.querySelector("body");
+
+// Executed when a checkbox is clicked, checks to see if the checkbox has the attribute checked,
+// if it does not, the function adds it to the checkbox, then calls toggleClass function with checkbox as argument,  // which is from the eventListener, so checkbox will either be darkTheme or largeText
+function addAttribute(checkbox) {
+  if (!checkbox.hasAttribute("checked")) {
+    checkbox.setAttribute("checked", true);
+    toggleClass(checkbox);
+  } else {
+    checkbox.removeAttribute("checked");
+    toggleClass(checkbox);
+  }
+}
+
+// Executed after a checkbox is clicked and addAttribute is run, checks to see which checkbox was clicked,
+// then toggles the appropriate class
+function toggleClass(checkbox) {
+  if (checkbox === darkTheme) {
+    bodyVar.classList.toggle("dark-theme-class")
+  } else {
+    bodyVar.classList.toggle("large-text-class")
+
+  }
+}
+
 document.querySelector("#message_id").addEventListener("keypress", (e) => {
   // console.log(e);
   if (e.key === "Enter") {
